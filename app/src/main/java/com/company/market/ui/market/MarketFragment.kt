6e7e6a549e45a35e03d12b071ab606d43f19a50a
Aditya Mahakalkar <adityamahakalkar49@gmail.com>
2020-04-26 01:31:12 +0530

    Profile screen ui implemented

--- app/src/main/java/com/company/market/ui/market/MarketFragment.kt
+++ app/src/main/java/com/company/market/ui/market/MarketFragment.kt
@@ -3,16 +3,24 @@ package com.company.market.ui.market
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.company.market.R
 import com.company.market.data.Product
 import com.company.market.databinding.FragmentMarketBinding
 
-class MarketFragment : Fragment() {
+class MarketFragment : Fragment(), Toolbar.OnMenuItemClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +29,7 @@ class MarketFragment : Fragment() {
         val binding = FragmentMarketBinding.inflate(inflater, container, false)
         binding.apply {
             toolbar.setupWithNavController(findNavController())
+            toolbar.setOnMenuItemClickListener(this@MarketFragment)
             recyclerView.apply {
                 val list: MutableList<Product> = mutableListOf()
                 repeat(50) {
@@ -38,5 +47,13 @@ class MarketFragment : Fragment() {
 
         }
         return binding.root
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return if (item?.itemId == R.id.menu_item_profile) {
+            findNavController().navigate(MarketFragmentDirections.actionMarketFragmentToProfileFragment())
+            true
+        } else false
+
     }
 }
