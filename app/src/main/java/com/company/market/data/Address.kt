45f68b68faf0b6d6acc61e7eb3a3e770c6f1085c
coditive <syrouscodive@gmail.com>
2020-04-28 09:13:16 +0530

    persisting user profile

--- app/src/main/java/com/company/market/data/Address.kt
+++ app/src/main/java/com/company/market/data/Address.kt
@@ -5,6 +5,7 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Address(
+
     val address_loc: String,
     val address_type: String,
     val address_pin: Int
