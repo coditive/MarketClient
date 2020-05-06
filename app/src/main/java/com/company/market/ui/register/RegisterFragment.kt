ca1a05923b5a7125381ac37bcb35bf79b52931ec
coditive <syrouscodive@gmail.com>
2020-05-06 18:29:57 +0530

    checks on input registration

--- app/src/main/java/com/company/market/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/company/market/ui/register/RegisterFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.company.market.MarketApplication
+import com.company.market.data.Address
+import com.company.market.data.UserSignUp
 import com.company.market.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
@@ -26,7 +29,23 @@ class RegisterFragment : Fragment() {
         val binding = FragmentRegisterBinding.inflate(inflater, container, false)
         binding.apply {
             buttonRegister.setOnClickListener {
-                viewModel.attemptRegister()
+                val (first , last) = editTextName.text.split(" ")
+                val email = editTextEmail.text.toString()
+                val phone =  editTextPhone.text.toString()
+                val loc = editTextAddressLoc.text.toString()
+                val pin = editTextAddressPincode.text.toString()
+
+                when {
+                    last.isEmpty() -> editTextName.error = "Enter Your Full Name"
+                    email.isEmpty() -> editTextEmail.error = "Email is required"
+                    phone.isEmpty() -> editTextPhone.error = "Phone Number is required"
+                    loc.isEmpty() -> editTextAddressLoc.error = "Address is required"
+                    pin.isEmpty() -> editTextAddressPincode.error = "Pincode is required"
+                    else ->
+                        viewModel.attemptRegister(UserSignUp(first, last, phone.toInt(), email,
+                            Address(loc, "home", pin.toInt())))
+                }
+                findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToMarketFragment())
             }
         }
         return binding.root
--- app/src/main/java/com/company/market/ui/register/RegisterVM.kt
+++ app/src/main/java/com/company/market/ui/register/RegisterVM.kt
@@ -3,15 +3,15 @@ package com.company.market.ui.register
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.company.market.data.UserSignUp
 import com.company.market.data.remote.RemoteApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class RegisterVM(private val remoteApi: RemoteApi) : ViewModel() {
-    fun attemptRegister(){
+    fun attemptRegister(userSignUp: UserSignUp){
         viewModelScope.launch(Dispatchers.IO) {
-
-            //TODO: insert register code here
+            remoteApi.createUser(userSignUp)
         }
     }
 }
